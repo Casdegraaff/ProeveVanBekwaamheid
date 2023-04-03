f6a73613647801af99fa3234a66269f2725f129d
Quinn Croese <Quinncroese@gmail.com>
2023-04-03 09:51:10 +0200

    Made it spawm a picture by first making it a 3d object

--- Assets/Scripts/EmailGame/SpawnEmail.cs
+++ Assets/Scripts/EmailGame/SpawnEmail.cs
@@ -5,18 +5,19 @@ using UnityEngine.UI;
 
 public class SpawnEmail : MonoBehaviour
 {
-    public Sprite Email; 
+    public GameObject Email; 
 
     Vector3 Spawnpoint;
     // Start is called before the first frame update
     void Start()
     {
-        Spawnpoint = new Vector3(0,0,0);
+        Spawnpoint = new Vector3(0,0,200);
     }
 
     public void EmailSpawn()
     {
-        Debug.Log("I wanne Die");
+        Debug.Log("Press");
         Instantiate( Email, Spawnpoint, Quaternion.identity);
+        Debug.Log("Second press");
     }
 }
