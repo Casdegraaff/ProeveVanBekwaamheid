6c1c201ec28c7bd7df4f57b29f2d6508253ada91
Quinn Croese <Quinncroese@gmail.com>
2023-04-03 12:53:39 +0200

    Fix for the scripts email spawn

--- Assets/Scripts/Emaildingen/SpawnEmail2.cs
+++ Assets/Scripts/Emaildingen/SpawnEmail2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpawnEmail : MonoBehaviour
+public class SpawnEmail2 : MonoBehaviour
 {
     // the button that u use
     public GameObject EmailButton;
