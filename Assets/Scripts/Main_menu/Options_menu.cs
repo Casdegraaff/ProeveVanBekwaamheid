85d65f95528fc2dbfb432f11502c543bbe27a49f
Quinn Croese <Quinncroese@gmail.com>
2023-04-05 10:52:01 +0200

    Added a fullscreen toggle and a volume slider

--- Assets/Scripts/Main_menu/Options_menu.cs
+++ Assets/Scripts/Main_menu/Options_menu.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Options_menu : MonoBehaviour
 {
+    // This is de audioMixer for the master volume
+    public AudioMixer audioMixer;
+
+    // Volume Settings
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
     // Makes that the game go fullscreen
     public void ToggleFullscreen()
     {
