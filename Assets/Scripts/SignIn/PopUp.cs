3c06e07868c748c1c61cfd1e97f3c51e22c90068
Quinn Croese <Quinncroese@gmail.com>
2023-04-20 10:12:26 +0200

    Update for the text spawn code and the text

--- Assets/Scripts/SignIn/PopUp.cs
+++ Assets/Scripts/SignIn/PopUp.cs
@@ -2,35 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class PopUp : MonoBehaviour
 {
-    //Is the inputfield for the emailAdress
-    public TMP_InputField InputEmail;
-
-    //Is the inputfield for the Password
-    public TMP_InputField InputPassword; 
-
-    //Email text what wil be filled in in inputfield
-    public string EmailText;
-
-    //Password text what wil be filled in in inputfield
-    public string PasswordText;
-
     //Warning text when button is Pressed
-    public Text Warning;
+    public GameObject Warning;
 
-    //This function is not build yet
-    public void EmailTextCheck()
+    //Start function when the game is started 
+    //Set the warning text false
+    public void Start()
     {
-        EmailText = InputEmail.text;
-        PasswordText = InputPassword.text;
+        Warning.SetActive(false);
     }
 
     //The function that spawns the text
     public void WarningText()
     {
-        Warning.enabled = true;
+        Warning.SetActive(true);
     }
 }
